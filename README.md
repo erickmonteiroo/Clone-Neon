d3be3866bbdc640e247a5d38ea857bce8e8eee52
Erick Monteiro <erickhenriquehmc@gmail.com>
2021-11-21 21:00:21 -0300

    21112021 - New branch for portfolio

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# Neon - Clone 
-### **Clone da landing page do [Banco Neon](https://neon.com.br) (Em Desenvolvimento)**
-Projeto desenvolvido com o intuito de exercitar os conhecimentos básicos e aprofundar os estudos em HTML e CSS.
+### **Portfolio PWM**
--- script.js
+++ script.js
@@ -2,33 +2,21 @@ async function getRandomUser() {
     const response = await fetch('https://randomuser.me/api/');
     const data = await response.json();
     const user = data.results[0];
-    displayUser(user);
+    displayInfo(user);
     console.log(data);
 }
 
-function displayUser(user) {
+function displayInfo(user) {
+    const copyright = document.getElementById('copyright')
+    const foto = document.getElementById('foto')
+    const local = document.getElementById('local')
     const nome = document.getElementById('nome')
-    const cpf = document.getElementById('cpf')
-    const celular = document.getElementById('celular')
-    const dia = document.getElementById('dia')
-    const mes = document.getElementById('mes')
-    const ano = document.getElementById('ano')
-    const email = document.getElementById('email')
 
-    nome.value = `${user.name.title} ${user.name.first} ${user.name.last}`
-    cpf.value = `${user.location.street.number}`
-    celular.value = `${user.phone}`
-    email.value = `${user.email}`
-    dia.value = getRandomInt(1, 31)
-    mes.value = getRandomInt(1, 12)
-    ano.value = getRandomInt(1900, 2021)
+    copyright.innerText = `2020, ${user.name.first} ${user.name.last}`
+    foto.src = `${user.picture.large}`
+    local.innerText = `${user.location.city}, ${user.location.country}`
+    nome.innerText = `${user.name.first} ${user.name.last}`
 }
-
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
 
 window.onload = function() {
     getRandomUser();
